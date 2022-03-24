5cc68862e188ac997ec024d11426862a171062c8
Pinkdonutj11 <102318075+Pinkdonutj11@users.noreply.github.com>
2022-03-24 17:31:31 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var box;
+
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite (200,200,20,20)
+  box.shapeColor = "blue"
+}
+
+function draw() 
+{
+  background(30);
+
+  if(keyDown("right")){
+    box.x = box.x  +3
+  }
+  if(keyDown("left")){
+    box.x = box.x -3
+  }
+  if(keyDown("up")){
+    box.y = box.y -3
+  }
+  if(keyDown("Down")){
+    box.y = box.y +3
+  }
+  drawSprites()
+
+}
+
+
+
+
